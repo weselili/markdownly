c94ae2fb66d0de31f9f4e17c5d31bdd0e6b56b52
Lili Boxer <ninaliboxer@gmail.com>
2019-08-29 14:15:43 -0700

    rendrered input

--- src/containers/AddDocContainer.js
+++ src/containers/AddDocContainer.js
@@ -1,12 +1,7 @@
 import { connect } from 'react-redux';
 import AddDoc from '../components/markdown/AddDoc';
-import { getTitle } from '../selectors/documentSelectors';
 
-const mapStateToProps = state => ({
-  title: getTitle(state)
-});
 
 
 export default connect(
-  mapStateToProps
 )(AddDoc);
