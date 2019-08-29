721cd38e4718f5d7f5df5d11ba8c6fce0d868253
Lili Boxer <ninaliboxer@gmail.com>
2019-08-29 11:19:40 -0700

    configs for travis

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+// eslint-disable-next-line
 module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
@@ -11,7 +12,7 @@ module.exports = {
   },
   plugins: [
     new HtmlPlugin({ template: './src/index.html' }),
-    new CleanPlugin('./dist')
+    new CleanWebpackPlugin('./dist')
   ],
   module: {
     rules: [
